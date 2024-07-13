02910ccb25863be069f380bd9d4e051b30ac51cd
TheKnownStranger <ajaykumar.c@wsddigital.com>
2024-07-13 22:15:14 +0530

    fix: help logic for args -h --help

--- pyls.py
+++ pyls.py
@@ -57,7 +57,9 @@ def print_ls(directory, show_hidden=False, reverse=False, sort_by_time=False, fi
     if reverse and not sort_by_time:
         items.reverse()
     for item in items:
-        print(item['name'])
+        print(item['name'],end= "  ")
+    print()
+    
 
 def print_ls_long(directory, show_hidden=False, reverse=False, sort_by_time=False, filter_type=None, human_readable=False):
     items = list_directory(directory, show_hidden, filter_type)
@@ -72,15 +74,14 @@ def print_ls_long(directory, show_hidden=False, reverse=False, sort_by_time=Fals
         time_modified = datetime.fromtimestamp(item['time_modified']).strftime('%b %d %H:%M')
         permissions = item['permissions']
         name = item['name']
-        print(f"{permissions} {size} {time_modified} {name}")
+        print(f"{permissions:<10} {size:>5} {time_modified} {name}")
 
 def print_file_long(file, human_readable=False):
     size = human_readable_size(file['size']) if human_readable else file['size']
     time_modified = datetime.fromtimestamp(file['time_modified']).strftime('%b %d %H:%M')
     permissions = file['permissions']
     name = file['name']
-    print(f"{permissions} {size} {time_modified} ./{name}")
-
+    print(f"{permissions:<10} {size:>5} {time_modified} ./{name}")
 def validate_filter(value):
     valid_filters = ['file', 'dir']
     if value not in valid_filters:
@@ -88,14 +89,15 @@ def validate_filter(value):
     return value
 
 def main():
-    parser = argparse.ArgumentParser(description='pyls - List directory contents.')
+    parser = argparse.ArgumentParser(description='pyls - List directory contents.', add_help=False)
     parser.add_argument('-A', action='store_true', help='do not ignore entries starting with .')
     parser.add_argument('-l', action='store_true', help='use a long listing format')
     parser.add_argument('-r', action='store_true', help='reverse order while sorting')
     parser.add_argument('-t', action='store_true', help='sort by modification time')
-    parser.add_argument('-H', action='store_true', help='show human-readable sizes')
+    parser.add_argument('-h', action='store_true', help='show human-readable sizes')
     parser.add_argument('--filter', type=validate_filter, help='filter by type (file or dir)')
     parser.add_argument('path', nargs='?', default='', help='path to list')
+    parser.add_argument('--help', action='help', help='show this help message and exit')
     args = parser.parse_args()
 
     file_path = 'structure.json'
@@ -111,9 +113,9 @@ def main():
 
     if args.l:
         if item_type == 'directory':
-            print_ls_long(item, show_hidden=args.A, reverse=args.r, sort_by_time=args.t, filter_type=args.filter, human_readable=args.H)
+            print_ls_long(item, show_hidden=args.A, reverse=args.r, sort_by_time=args.t, filter_type=args.filter, human_readable=args.h)
         elif item_type == 'file':
-            print_file_long(item, human_readable=args.H)
+            print_file_long(item, human_readable=args.h)
     else:
         if item_type == 'directory':
             print_ls(item, show_hidden=args.A, reverse=args.r, sort_by_time=args.t, filter_type=args.filter)
@@ -122,4 +124,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
