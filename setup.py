b582f3fc874a36b0196b1e9a09bdd09380278cbf
TheKnownStranger <ajaykumar.c@wsddigital.com>
2024-07-14 07:30:55 +0530

    fix: fix the no module found issue in setup.py for pyls

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='pyls',
     version='1.0',
-    packages=find_packages(),
+    py_modules=['pyls'],
     entry_points={
         'console_scripts': [
-            'pyls = pyls:main'
-        ]
+            'pyls=pyls:main',
+        ],
     },
-    python_requires='>=3.6',
+    author='Ajay Kumar Ch',
+    author_email='ajaykumarch1979@gmail.com',
+    description='A simple command line tool',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
 )
